96624e5bb9466f03d499a759de85a0210e7c34e7
Andrew Thompson <mrwerdo331@me.com>
2016-04-03 14:33:17 +1000

    Implemented TIFFImage, which can only write atm.

--- Source/main.swift
+++ Source/main.swift
@@ -1,42 +1,35 @@
 import CLibTIFF
 
-var tif = TIFFOpen("/Users/mrwerdo/Developer/Fractals/redesigned-palm-tree-fractals/Fractal.tiff", "w")
-if tif != nil {
-	var width: UInt32 = 100
-	var height: UInt32 = 100
-	let size = Int(width * height * 4)
+private let size = Size(100, 100)
+private var buffer = [UInt8](count: size.height * size.width * 3, repeatedValue: 0)
 
-	TIFFSetField_uint32(tif, UInt32(TIFFTAG_IMAGEWIDTH), width)
-	TIFFSetField_uint32(tif, UInt32(TIFFTAG_IMAGELENGTH), height)
-	TIFFSetField_uint32(tif, UInt32(TIFFTAG_BITSPERSAMPLE), 8)
-	TIFFSetField_uint32(tif, UInt32(TIFFTAG_SAMPLESPERPIXEL), 4)
-	TIFFSetField_uint32(tif, UInt32(TIFFTAG_ROWSPERSTRIP), 1)
+//var counter = 0
+//for y in 0..<size.height {
+//	for x in 0.stride(to: size.height, by: 4) {
+//		switch counter % 4 {
+//		case 0: buffer[y * size.width + x + 0] = 255 // Red
+//		case 1: buffer[y * size.width + x + 1] = 255 // Green
+//		case 2: buffer[y * size.width + x + 2] = 255 // Blue
+//		default: break
+//		}
+//	}
+//}
 
-	var extraChannels: [UInt16] = [UInt16(EXTRASAMPLE_ASSOCALPHA)]
-	TIFFSetField_ExtraSample(tif, 1, &extraChannels)
-	
-	TIFFSetField_uint32(tif, UInt32(TIFFTAG_PHOTOMETRIC), UInt32(PHOTOMETRIC_RGB))
-	TIFFSetField_uint32(tif, UInt32(TIFFTAG_PLANARCONFIG), UInt32(PLANARCONFIG_CONTIG))
-	TIFFSetField_uint32(tif, UInt32(TIFFTAG_ORIENTATION), UInt32(ORIENTATION_TOPLEFT))
-
-	var buffer = [[UInt8]](count: Int(height), repeatedValue: [UInt8](count: Int(width * 4), repeatedValue: 0))
-
-	for y in 0..<Int(height) {
-		for x in 0.stride(to: Int(width * 4), by: 4) {
-			buffer[y][x] = UInt8(x % 255)
-			buffer[y][x+2] = UInt8((Int(width * 4) - x) % 255)
-			buffer[y][x+1] = 0
-			buffer[y][x+3] = 255 // alpha
+for y in 0..<size.height {
+	for x in 0.stride(to: Int(size.width * 3), by: 3) {
+		switch x % 9 {
+		case 0:
+			buffer[y * size.width * 3 + x] = 255
+		case 3:
+			buffer[y * size.width * 3 + x + 1] = 255
+		case 6:
+			buffer[y * size.width * 3 + x + 2] = 255
+		default:
+			break
 		}
 	}
-
-	for row in 0..<Int(height) {
-		var k: [UInt8] = buffer[row]
-		if TIFFWriteScanline(tif, &k, UInt32(row), 0) < 0 {
-			print("Error!")
-		}
-	}
-
-	print("The size is: \(width)x\(height)")
 }
-TIFFClose(tif)
+private let path = "/Users/mrwerdo/Developer/Fractals/redesigned-palm-tree-fractals/Fractal.tiff"
+private var image: TIFFImage = try TIFFImage(writeAt: path, &buffer, size, hasAlpha: false)
+image.ownsBuffer = false
+try image.write()
