50fcf3a0ee6300c4366324e04ed251ea727b7c62
Andrew Thompson <mrwerdo331@me.com>
2016-05-07 17:20:12 +1000

    Package description ignores correct base files.

--- Package.swift
+++ Package.swift
@@ -3,5 +3,5 @@ import PackageDescription
 let pacakge = Package(
 	name: "redesigned-palm-tree-fractals",
 	dependencies: [.Package(url: "../CLibTIFF/.git", majorVersion: 1)],
-	exclude: ["Sources", "LICENCE", "README.md"]
+	exclude: ["Sources", "LICENCE", "README.md", "Fractal.tiff", "bin/", "makefile"]
 )
