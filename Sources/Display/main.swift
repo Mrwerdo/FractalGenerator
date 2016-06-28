0505bb025e8a2660dd7cd58b7f9a97d297484a37
Andrew Thompson <mrwerdo331@me.com>
2016-06-29 09:44:45 +1000

    This combination, is like, wow!
    Made by Filip Reierson

--- Sources/Display/main.swift
+++ Sources/Display/main.swift
@@ -3,11 +3,38 @@ import Geometry
 import Process
 import Cocoa
 
-let mandelbrotSet = MandelbrotSet(numberOfIterations: 2000)
-
 func **(lhs: Int, rhs: Int) -> Int {
   return Int(pow(Double(lhs), Double(rhs)))
 }
+
+func af(Z: Complex) -> Complex {
+    return Z*Z + Complex(0.835,-0.2321)
+}
+
+func bf(Z: Complex) -> Complex {
+    return Z*Z + 0.279
+}
+
+struct Zipper : FComputer {
+    var numberOfIterations: Int
+    var ac: JuliaSet
+    var bc: JuliaSet
+    
+    init(numberOfIterations: Int) {
+        self.numberOfIterations = numberOfIterations
+        ac = JuliaSet(numberOfIterations: numberOfIterations, function: af)
+        bc = JuliaSet(numberOfIterations: numberOfIterations, function: bf)
+    }
+
+    func computerPoint(C: Complex) -> Int {
+        let a = ac.computerPoint(C: C)
+        let b = bc.computerPoint(C: C)
+        return Int(abs(Double(b)*Double(a) - pow(Double(a),Double(b)*0.02)))
+    }
+}
+
+let mandelbrotSet = Zipper(numberOfIterations: 4000)
+
 
 public struct ModulusColorizerCGFloat: FColorizer {
     public typealias Channel = UInt16
@@ -36,3 +63,4 @@ let app = FAppDelegate(frame: frame)
 let v = FViewController("MandelbrotSet", frame, mandelbrotSet, colorizer)
 app.controller = v
 app.run()
+
